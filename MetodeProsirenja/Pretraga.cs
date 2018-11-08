0a21fc890a8494576d57609013b5bbf765de946f
Darjan Kusan <600zgb@windowslive.com>
2018-11-08 20:58:25 +0100

    Rijesili smo pretragu

--- MetodeProsirenja/Pretraga.cs
+++ MetodeProsirenja/Pretraga.cs
@@ -9,22 +9,19 @@ namespace Vsite.CSharp.KontrolaToka
     {
         public static bool Sadrži(IEnumerable<string> nizRiječi, string tražena)
         {
-            // TODO:121 Napisati kod koji vratiti true ako nizRiječi sadrži riječ tražena, a u protivnom vraća false
-
+            return nizRiječi.Contains(tražena);
             throw new NotImplementedException();
         }
 
         public static int Zbroj(IEnumerable<int> brojevi)
         {
-            // TODO:122 Napisati kod koji će zbrojiti sve članove kolekcije cijelih brojeva i vratiti taj zbroj kao rezultat
-
+            return brojevi.Sum();
             throw new NotImplementedException();
         }
 
         public static IEnumerable<string> NađiSveIza(IEnumerable<string> nizRiječi, string graničnaRiječ)
         {
-            // TODO:123 Napisati kod koji će naći sve riječi iz kolekcije nizRiječi koje su abecedno iza riječi graničnaRiječ
-
+            return nizRiječi.Where(riječ => riječ.CompareTo(graničnaRiječ) > 0);
             throw new NotImplementedException();
         }
     }
